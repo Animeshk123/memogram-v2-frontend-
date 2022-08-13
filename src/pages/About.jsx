91bcfa0ee7040069e03ea63d90d75696860a0d7a
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-08-13 11:04:42 +0530

    Delete src/pages directory

--- src/pages/About.jsx
+++
@@ -1,5 +0,0 @@
-export default function About(){
-  return <iframe src="https://animeshkumbhakar.netlify.app"
-    className="w-full h-screen"
-    title="about"></iframe>;
-}
--- src/pages/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-  return(
-      <div className="w-24 h-24">
-       <img src="/giphy.gif" className="rounded-full w-full h-full object-cover object-center" alt="loader"/>
-      </div>
-    )
-}
